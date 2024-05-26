0c2d5c69dfb0f0a9f83dbb1739486fc0fce3e13c
Debo Jyoti Paul <87137515+DeboJp@users.noreply.github.com>
2024-05-26 12:37:57 -0500

    Mongo Initial commit

--- pages/api/drugs.js
+++ pages/api/drugs.js
@@ -1,9 +1,18 @@
-import fs from 'fs';
-import path from 'path';
+// import fs from 'fs';
+// import path from 'path';
 
-export default function handler(req, res) {
-  const filePath = path.resolve('public', 'drug_list.json');
-  const jsonData = fs.readFileSync(filePath, 'utf8');
-  const data = JSON.parse(jsonData);
-  res.status(200).json(data);
+// export default function handler(req, res) {
+//   const filePath = path.resolve('public', 'drug_list.json');
+//   const jsonData = fs.readFileSync(filePath, 'utf8');
+//   const data = JSON.parse(jsonData);
+//   res.status(200).json(data);
+// }
+import clientPromise from '../../lib/mongodb';
+
+export default async function handler(req, res) {
+  const client = await clientPromise;
+  const db = client.db('Cluster0');
+  const collection = db.collection('drug_list');
+  const drugs = await collection.find({}).toArray();
+  res.status(200).json(drugs);
 }
